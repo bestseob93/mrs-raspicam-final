9cfe139405053af0ae53c701dc9fbe28b7c7e504
Hwan Seob Lee <hwanse@hwanseui-MacBook-Pro.local>
2017-05-21 21:37:04 +0900

    added stdinReadable

--- src/index.js
+++ src/index.js
@@ -73,3 +73,12 @@ faceCam.on('start', function(err, timestamp) {
 });
 
 startFaceStreaming();
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', () => {
+		  var chunk = process.stdin.read();
+			if(chunk !== null) {
+				process.stdout.write(`data: ${chunk}`);
+		  }
+});
